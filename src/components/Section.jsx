ad589f735818760f8a710d0fe90c468ce3f4d446
Enric <enricgalerarotger@gmail.com>
2026-06-02 13:27:52 +0200

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,30 @@ import GridCard from "./GridCard";
 /** Returns a section tag
  * @param {{ data: { id: string } }} props
  */
-const SectionJSX = ({ data }) => {
+const SectionJSX = ({ sectId, data }) => {
+    const GridDiv = (
+        <div className="grid">
+            <GridCard data_index={"1"} />
+            <GridCard data_index={"2"} />
+            <GridCard data_index={"3"} />
+            <GridCard data_index={"4"} />
+            <GridCard data_index={"5"} />
+            <GridCard data_index={"6"} />
+        </div>
+    );
+    const ListDiv = (
+        <>
+            <div className="list">
+                <ul>
+                    <li>Guaranteed Quality</li>
+                    <li>Deliver in less than 5 working days</li>
+                    <li>Cheap &#38; Nice</li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    );
+    const ProductsDiv = <></>;
     return (
         <section id={data.id} className="bodysection" style={{ backgroundColor: data.bgcolor }}>
             <div className="pctitle">
@@ -11,14 +34,10 @@ const SectionJSX = ({ data }) => {
             </div>
             <div className="pctext pctext-grid">
                 {/* <p className="ptext">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p> */}
-                <div className="grid">
-                    <GridCard data_index={"1"} />
-                    <GridCard data_index={"2"} />
-                    <GridCard data_index={"3"} />
-                    <GridCard data_index={"4"} />
-                    <GridCard data_index={"5"} />
-                    <GridCard data_index={"6"} />
-                </div>
+                {sectId === 1 && <GridDiv />}
+                {sectId === 2 && <ListDiv />}
+                {sectId === 3 && <ProductsDiv />}
+                {sectId === 4 && <Footer />}
             </div>
         </section>
     )
